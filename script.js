225183e8c36d699eea51d76a1f8119e94286f36f
Sean O'Mahoney <seandavidomahoney@gmail.com>
2017-12-28 05:26:35 +0000

    Search Events

--- script.js
+++ script.js
@@ -22,7 +22,10 @@ var deselect;
 var groupsContainer;
 var eventsContainer;
 
-let gSelected = 0; // Bodge for one selected meetup group
+var eventSearch;
+var searchEvents;
+
+let gSelected = 10; // Bodge for one selected meetup group, needs to be more than 1 for search
 
 const meetups = ["android_mcr", "BCS-Greater-Manchester-Branch", "blabtalks", "meetup-group-wHuRVtrk", "CIA-Chicks-in-Advertising-Manchester", "Code-Your-Future-Manchester", "CodeUpManchester", "CodeUp-Salford", "Digital-Analytics-Manchester", "Digital_North_", "DotNetNorth", "Enterprise-UX", "freelance-folk-manchester", "HackerNestMAN", "hackspace-manchester", "HadoopManchester", "HCD-Manchester", "IoTMCR", "JavaScript-North-West", "Ladies-of-Code-Manchester", "Lean-Agile-Manchester", "MaccTech", "Magento-Minds-of-Manchester", "MancDB", "Manchester-Bitcoin-blockchain-and-other-cryptocurrencies", "Manchester-Angular-Workshop", "manchesterentrepreneursclub", "Manchester-Futurists", "Manchester-Grey-Hats", "Manchester-InfoSec", "ManchesterUK-Java-Community", "Power-BI-Manchester-Meetup", "Manchester-R", "Manchester-React-User-Group", "ManchesterWordPressUserGroup", "MancJS", "McrFRED", "McrUXD", "Messaging-Bots-Manchester", "Music-Culture-and-Technology", "Neo4j-Manchester", "North-West-IT-Crowd-Beer-BBQ-Event", "North-West-Ruby-User-Group", "Open-Data-Manchester", "Practical-Business-Workshops-Manchester", "RealUX", "Salford-Lean-Startup", "scala-developers", "SEO-Manchester", "Social-Software-Development-Meetup-in-Manchester", "Tech-for-Good-Live", "Tech-Leads-NW", "Test-Hive-Manchester", "ThoughtWorks-Manchester-Events", "UK-North-Crypto-Currency-Meetup", "The-UX-Crunch-North", "VRManchester", "AWS-User-Group-North", "Code-Nation", "Manchester-Open-Source", "DevOps-Manchester", "StartupBlink-Manchester", "nwdrupal", "Manchester-Xamarin-User-Group", "manchester-node-workshop", "DATA-VISUALISATION-MEETUP", "BlockchainManchesterMeetup", "Manchester-WordPress-SEO-Startup", "Manchester-Unity3D-Game-Dev-Meetup", "Google-Cloud-Platform-Users-North-West", "Manc-Bitcoin", "craftcmsmanchester", "Couchbase-Manchester", "NSManchester", "Python-North-West-Meetup", "Expert-Talks-Manchester"]; // wHuRVtrk = Blockchain, PJzMkpIw = Cyber Security Seminar, "meetup-group-PJzMkpIw"
 
@@ -42,13 +45,16 @@ function init() {
     deselect = document.getElementById("deselect");
     groupsContainer = document.getElementById("groupsContainer");
     eventsContainer = document.getElementById("eventsContainer");
+    eventSearch = document.getElementById("eventSearch");
+    searchEvents = document.getElementById("searchEvents");
 
     generate.addEventListener("click", function () {
-        generateCalendar(getMeetupsFromIndexes(getSelectedMeetupsIndexes()));
+        generateCalendar(getMeetupsFromIndexes(getSelectedMeetupsIndexes()), "");
     });
-    searchBox.addEventListener("keyup", searchMeetups); 
+    searchBox.addEventListener("keyup", searchMeetups);
     select.addEventListener("click", selectAllShown);
     deselect.addEventListener("click", deselectAllShown);
+    searchEvents.addEventListener("click", searchEventsFor);
 
     initGetMeetups();
 }
@@ -79,57 +85,68 @@ function initGetMeetups() {
         });
 }
 
-function generateCalendar(x) {
-    eventsJSON = x; // replace with selected
+function searchEventsFor() {
+    generateCalendar(meetups, eventSearch.value.toUpperCase());
+}
+
+// t = term
+function generateCalendar(x, t) {
+    eventsJSON = x; // x = Meetups Array
     eventsJSON = eventsJSON.map(app.getEvents);
 
     $.when(...eventsJSON)
         .then((...eventsJSON) => {
-        if (gSelected > 1) {
-            eventsJSON = eventsJSON.map(a => a[0].data)
-                .reduce((prev, curr) => [...prev, ...curr], [])
-                .sort(function (a, b) {
-                    return a.time - b.time;
-                });
-    } else {
-              eventsJSON = eventsJSON.map(a => a.data)[0];
-              }
+            if (gSelected > 1) {
+                eventsJSON = eventsJSON.map(a => a[0].data)
+                    .reduce((prev, curr) => [...prev, ...curr], [])
+                    .sort(function (a, b) {
+                        return a.time - b.time;
+                    });
+            } else {
+                eventsJSON = eventsJSON.map(a => a.data)[0];
+            }
             console.log(eventsJSON);
-            drawCalendar(eventsJSON);
+            drawCalendar(eventsJSON, t);
         });
 }
 
 
-function drawCalendar(JSON) {
+function drawCalendar(JSON, t) {
     // JSON = JSON.sort(function (a, b) { return a.time > b.time; });
     eventsContainer.innerHTML = "";
     var month = '00';
     for (var i = 0; i < JSON.length; i++) {
         var x = JSON[i];
+        var desc = x.description.replace(/<(?:.|\n)*?>/gm, '').toUpperCase();
+        var name = x.name.toUpperCase();
+        
+        if (desc.includes(t) || name.includes(t)) {
 
-        var eventName = x.name;
-        var eventLink = x.link;
-        var groupName = x.group.name;
-        var groupLink = "https://www.meetup.com/" + x.group.urlname + "/";
-        var date = x.local_date;
-        var day = date.substring(8, 10);
-        var year = date.substring(0, 4);
-        var time = (x.hasOwnProperty('local_time')) ? x.local_time : "N/A";
-        var rsvp = x.yes_rsvp_count;
-        var rsvpLimit = (x.hasOwnProperty('rsvp_limit')) ? x.rsvp_limit : "∞";
+            var eventName = x.name;
+            var eventDesc = x.description.replace(/<(?:.|\n)*?>/gm, '')
+            var eventLink = x.link;
+            var groupName = x.group.name;
+            var groupLink = "https://www.meetup.com/" + x.group.urlname + "/";
+            var date = x.local_date;
+            var day = date.substring(8, 10);
+            var year = date.substring(0, 4);
+            var time = (x.hasOwnProperty('local_time')) ? x.local_time : "N/A";
+            var rsvp = x.yes_rsvp_count;
+            var rsvpLimit = (x.hasOwnProperty('rsvp_limit')) ? x.rsvp_limit : "∞";
 
-        var venueName = (x.hasOwnProperty('venue')) ? x.venue.name : "N/A";
-        var venueAddress = (x.hasOwnProperty('venue')) ? x.venue.address_1 : "";
-        var venuePostcode = (x.hasOwnProperty('venue')) ? x.venue.city : "";
+            var venueName = (x.hasOwnProperty('venue')) ? x.venue.name : "N/A";
+            var venueAddress = (x.hasOwnProperty('venue')) ? x.venue.address_1 : "";
+            var venuePostcode = (x.hasOwnProperty('venue')) ? x.venue.city : "";
 
-        var event = '<div class="event"><div class="numbers"><p class="day">' + ordinalSuffix(day) + '</p><p>' + timeConvert(time) + '</p><p>' + rsvp + '/' + rsvpLimit + '</p></div><div class="details"><a href="' + eventLink + '"><h4>' + eventName + '</h4></a><p>' + venueName + ' - ' + venueAddress + ' (' + venuePostcode + ')' + '</p><a href="' + groupLink + '"><p>' + groupName + '</p></a></div>';
+            var event = '<div class="event"><div class="numbers"><p class="day">' + ordinalSuffix(day) + '</p><p>' + timeConvert(time) + '</p><p>' + rsvp + '/' + rsvpLimit + '</p></div><div class="details"><a href="' + eventLink + '"><h4>' + eventName + '</h4></a><p>' + venueName + ' - ' + venueAddress + ' (' + venuePostcode + ')' + '</p><a href="' + groupLink + '"><p>' + groupName + '</p></a></div>';
 
-        if (month != date.substring(5, 7)) {
-            month = date.substring(5, 7);
-            eventsContainer.insertAdjacentHTML('beforeend', '<h3 class="month">' + months.get(month) + ' (' + year + ')' + '</h3>')
+            if (month != date.substring(5, 7)) {
+                month = date.substring(5, 7);
+                eventsContainer.insertAdjacentHTML('beforeend', '<h3 class="month">' + months.get(month) + ' (' + year + ')' + '</h3>')
+            }
+
+            eventsContainer.insertAdjacentHTML('beforeend', event);
         }
-
-        eventsContainer.insertAdjacentHTML('beforeend', event);
     }
 }
 
@@ -260,13 +277,14 @@ function ordinalSuffix(i) {
 function timeConvert(i) {
     var ampm;
     var x;
-    if (i.substring(0,2) > 12) {
+    if (i.substring(0, 2) > 12) {
         ampm = "PM";
-        x = i.substring(0,2) % 12 + ":" + i.substring(3,5) + ampm;
+        x = i.substring(0, 2) % 12 + ":" + i.substring(3, 5) + ampm;
     } else {
         ampm = "AM";
-        x = i.substring(0,2) + ":" + i.substring(3,5) + ampm;
-    } return x;
+        x = i.substring(0, 2) + ":" + i.substring(3, 5) + ampm;
+    }
+    return x;
 }
 
 function setupButtons() {
