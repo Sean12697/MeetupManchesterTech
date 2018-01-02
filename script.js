cbe84f7aaf87932f74007c0e8a12c64baac1618a
Sean O'Mahoney <seandavidomahoney@gmail.com>
2018-01-02 19:38:31 +0000

    TechNW Only Generate

--- script.js
+++ script.js
@@ -30,11 +30,14 @@ var eventsContainer;
 var eventSearch;
 var searchEvents;
 
+var techNW;
+var generateAll;
+
 let gSelected = 10; // Bodge for one selected meetup group, needs to be more than 1 for search
 
-const meetups = ["android_mcr", "BCS-Greater-Manchester-Branch", "blabtalks", "meetup-group-wHuRVtrk", "CIA-Chicks-in-Advertising-Manchester", "Code-Your-Future-Manchester", "CodeUpManchester", "CodeUp-Salford", "Digital-Analytics-Manchester", "Digital_North_", "DotNetNorth", "Enterprise-UX", "freelance-folk-manchester", "HackerNestMAN", "hackspace-manchester", "HadoopManchester", "HCD-Manchester", "IoTMCR", "JavaScript-North-West", "Ladies-of-Code-Manchester", "Lean-Agile-Manchester", "MaccTech", "Magento-Minds-of-Manchester", "MancDB", "Manchester-Bitcoin-blockchain-and-other-cryptocurrencies", "Manchester-Angular-Workshop", "manchesterentrepreneursclub", "Manchester-Futurists", "Manchester-Grey-Hats", "Manchester-InfoSec", "ManchesterUK-Java-Community", "Power-BI-Manchester-Meetup", "Manchester-R", "Manchester-React-User-Group", "ManchesterWordPressUserGroup", "MancJS", "McrFRED", "McrUXD", "Messaging-Bots-Manchester", "Music-Culture-and-Technology", "Neo4j-Manchester", "North-West-IT-Crowd-Beer-BBQ-Event", "North-West-Ruby-User-Group", "Open-Data-Manchester", "Practical-Business-Workshops-Manchester", "RealUX", "Salford-Lean-Startup", "scala-developers", "SEO-Manchester", "Social-Software-Development-Meetup-in-Manchester", "Tech-for-Good-Live", "Tech-Leads-NW", "Test-Hive-Manchester", "ThoughtWorks-Manchester-Events", "UK-North-Crypto-Currency-Meetup", "The-UX-Crunch-North", "VRManchester", "AWS-User-Group-North", "Code-Nation", "Manchester-Open-Source", "DevOps-Manchester", "StartupBlink-Manchester", "nwdrupal", "Manchester-Xamarin-User-Group", "manchester-node-workshop", "DATA-VISUALISATION-MEETUP", "BlockchainManchesterMeetup", "Manchester-WordPress-SEO-Startup", "Manchester-Unity3D-Game-Dev-Meetup", "Google-Cloud-Platform-Users-North-West", "Manc-Bitcoin", "craftcmsmanchester", "Couchbase-Manchester", "NSManchester", "Python-North-West-Meetup", "Expert-Talks-Manchester", "HER-Data-MCR", "northernsoho"]; // wHuRVtrk = Blockchain
+const meetups = ["android_mcr", "BCS-Greater-Manchester-Branch", "blabtalks", "meetup-group-wHuRVtrk", "CIA-Chicks-in-Advertising-Manchester", "Code-Your-Future-Manchester", "CodeUpManchester", "CodeUp-Salford", "Digital-Analytics-Manchester", "Digital_North_", "DotNetNorth", "Enterprise-UX", "freelance-folk-manchester", "HackerNestMAN", "hackspace-manchester", "HadoopManchester", "HCD-Manchester", "IoTMCR", "JavaScript-North-West", "Ladies-of-Code-Manchester", "Lean-Agile-Manchester", "MaccTech", "Magento-Minds-of-Manchester", "MancDB", "Manchester-Bitcoin-blockchain-and-other-cryptocurrencies", "Manchester-Angular-Workshop", "manchesterentrepreneursclub", "Manchester-Futurists", "Manchester-Grey-Hats", "Manchester-InfoSec", "ManchesterUK-Java-Community", "Power-BI-Manchester-Meetup", "Manchester-R", "Manchester-React-User-Group", "ManchesterWordPressUserGroup", "MancJS", "McrFRED", "McrUXD", "Messaging-Bots-Manchester", "Music-Culture-and-Technology", "Neo4j-Manchester", "North-West-IT-Crowd-Beer-BBQ-Event", "North-West-Ruby-User-Group", "Open-Data-Manchester", "Practical-Business-Workshops-Manchester", "RealUX", "Salford-Lean-Startup", "scala-developers", "SEO-Manchester", "Social-Software-Development-Meetup-in-Manchester", "Tech-for-Good-Live", "Tech-Leads-NW", "Test-Hive-Manchester", "ThoughtWorks-Manchester-Events", "UK-North-Crypto-Currency-Meetup", "The-UX-Crunch-North", "VRManchester", "AWS-User-Group-North", "Code-Nation", "Manchester-Open-Source", "DevOps-Manchester", "StartupBlink-Manchester", "nwdrupal", "Manchester-Xamarin-User-Group", "manchester-node-workshop", "DATA-VISUALISATION-MEETUP", "BlockchainManchesterMeetup", "Manchester-WordPress-SEO-Startup", "Manchester-Unity3D-Game-Dev-Meetup", "Google-Cloud-Platform-Users-North-West", "Manc-Bitcoin", "craftcmsmanchester", "Couchbase-Manchester", "NSManchester", "Python-North-West-Meetup", "Expert-Talks-Manchester", "HER-Data-MCR", "northernsoho", "North-West-Playtesters"]; // wHuRVtrk = Blockchain
 
-const months = new Map([['01', 'January'], ['02', 'February'], ['03', 'March'], ['04', 'April'], ['05', 'May'], ['06', 'June'], ['07', 'July'], ['08', 'August'], ['09', 'September'], ['10', 'October'], ['11', 'November'], ['12', 'December']]);
+const months = new Map([[1, 'January'], [2, 'February'], [3, 'March'], [4, 'April'], [5, 'May'], [6, 'June'], [7, 'July'], [8, 'August'], [9, 'September'], [10, 'October'], [11, 'November'], [12, 'December']]);
 
 var eventsJSON = meetups;
 var MeetupsJSON = meetups;
@@ -43,6 +46,7 @@ function init() {
     meetups.sort(function (a, b) {
         return a.toLowerCase().localeCompare(b.toLowerCase())
     });
+
     initDOMelements();
     initGetMeetups();
 }
@@ -100,9 +104,8 @@ function generateCalendar(x, t) {
 
 
 function drawCalendar(JSON, t) {
-    // JSON = JSON.sort(function (a, b) { return a.time > b.time; });
     eventsContainer.innerHTML = "";
-    var month = '00';
+    var month = 0;
     for (var i = 0; i < JSON.length; i++) {
         var x = JSON[i];
         var desc = (x.hasOwnProperty('description')) ? x.description.replace(/<(?:.|\n)*?>/gm, '').toUpperCase() : ""; // Because some Meetups do not have desc, founded and stopped rendering for "NSManchester: iOS Developer Group" due to error
@@ -141,9 +144,9 @@ function drawCalendar(JSON, t) {
 
             var event = '<div class="event"><div class="numbers"><p class="day">' + ordinalSuffix(day) + '</p><p>' + timeRange + '</p><p>' + rsvp + '/' + rsvpLimit + '</p></div><div class="details"><a href="' + eventLink + '" target="_blank"><h4>' + eventName + '</h4></a><p>' + venueName + ' - ' + venueAddress + ' (' + venuePostcode + ')' + '</p><a href="' + groupLink + '" target="_blank"><p>' + groupName + '</p></a></div>';
 
-            if (month != date.substring(5, 7)) {
-                month = date.substring(5, 7);
-                eventsContainer.insertAdjacentHTML('beforeend', '<h3 class="month">' + months.get(month) + ' (' + year + ')' + '</h3>')
+            if (month != parseInt(date.substring(5, 7))) {
+                month = parseInt(date.substring(5, 7));
+                eventsContainer.insertAdjacentHTML('beforeend', '<h3 class="month">' + months.get(parseInt(month)) + ' (' + year + ')' + '</h3>')
             }
 
             eventsContainer.insertAdjacentHTML('beforeend', event);
@@ -240,6 +243,85 @@ app.getMeetups = (meetup) => $.ajax({
     method: 'GET',
     dataType: 'jsonp'
 });
+
+function getTechNW(merge, t) {
+    var r = new XMLHttpRequest();
+
+    r.open('GET', 'https://www.googleapis.com/calendar/v3/calendars/a73q3trj8bssqjifgolb1q8fr4@group.calendar.google.com/events?key=AIzaSyCR3-ptjHE-_douJsn8o20oRwkxt-zHStY&maxResults=9999&singleEvents=true&orderBy=starttime', true);
+
+    r.onload = () => {
+        var data = JSON.parse(r.responseText);
+        console.log(data);
+        data = data.items;
+
+        var futureEvents = [];
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].hasOwnProperty('start')) {
+                var s = data[i].start;
+                if (s.hasOwnProperty('dateTime')) {
+                    if (!pastEvent(s.dateTime)) {
+                        futureEvents.push(data[i]);
+                    }
+                }
+            }
+        }
+
+        futureEvents.sort(function (a, b) {
+            return new Date(a.start.dateTime).getTime() - new Date(b.start.dateTime).getTime();
+        });
+
+        if (merge) generateAllEvents(futureEvents);
+        if (!merge) drawTechNW(futureEvents);
+
+        console.log(futureEvents);
+    }
+
+    r.send();
+}
+
+function generateAllEvents(techNWMeetups) {
+    
+}
+
+function drawTechNW(JSON) {
+    document.getElementById("eventsContainer").innerHTML = "";
+    var m = 0;
+    for (var i = 0; i < JSON.length; i++) {
+        var x = JSON[i];
+        // console.log(x);
+        var name = x.summary;
+        var date = x.start.dateTime;
+        var day = date.substr(8, 2);
+        var month = new Date(date).getMonth() + 1;
+        var year = new Date(date).getYear();
+        year = '20' + year.toString().substring(1, 3);
+        var h = (new Date(date).getHours() > 9) ? new Date(date).getHours() : '0' + new Date(date).getHours();
+        var min = (new Date(date).getMinutes() > 9) ? new Date(date).getMinutes() : '0' + new Date(date).getMinutes();
+        var time = h + ":" + min;
+        var location = (x.hasOwnProperty('location')) ? x.location : "N/A";
+        var link = x.htmlLink;
+
+        var event = '<div class="event"><div class="numbers"><p class="day">' + ordinalSuffix(day) + '</p><p>' + timeConvert(time) + '</p></div><div class="details"><a href="' + link + '" target="_blank"><h4>' + name + '</h4></a><p>' + location + '</p><a href="http://technw.uk/calendar" target="_blank"><p> TechNW </p></a></div>';
+
+        if (m != month) {
+                m = month;
+                eventsContainer.insertAdjacentHTML('beforeend', '<h3 class="month">' + months.get(month) + ' (' + year + ')' + '</h3>')
+            }
+        
+        eventsContainer.insertAdjacentHTML('beforeend', event);
+    }
+}
+
+function pastEvent(date) {
+    var compareDate = new Date(date);
+    var now = new Date();
+    
+    if (now.getTime() > compareDate.getTime()) {
+        return true;
+    }
+
+    return false;
+};
 
 function showEvents(indexes) {
     var group = document.getElementsByClassName("group");
@@ -432,6 +514,8 @@ function initDOMelements() {
     eventsContainer = document.getElementById("eventsContainer");
     eventSearch = document.getElementById("eventSearch");
     searchEvents = document.getElementById("searchEvents");
+    techNW = document.getElementById("techNW");
+    generateAll = document.getElementById("generateAll");
 
     generate.addEventListener("click", function () {
         generateCalendar(getMeetupsFromIndexes(getSelectedMeetupsIndexes()), "");
@@ -447,4 +531,10 @@ function initDOMelements() {
     invertSelect.addEventListener("click", invertSelection);
 
     searchEvents.addEventListener("click", searchEventsFor);
+    techNW.addEventListener("click", function () {
+        getTechNW(false, "");
+    });
+    generateAll.addEventListener("click", function () {
+        getTechNW(true, "");
+    });
 }
